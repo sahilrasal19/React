2fdcf4a6edcd7dd2f901111787250255746c8c9a
SAHIL <sahilrasal742@gmail.com>
2025-06-28 18:18:59 +0530

    ignite app

--- Apps.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement(
-    "h1", //tag creation
-    {id : "heading" , xyz : "sahil"}, // used to define attributes
-    "SAHIL GREATEST CODER" //text content or what we want inside the tag can be text,another element anything
-); //creates object and html tag for react
-
-        const root = ReactDOM.createRoot(document.getElementById("root")); //creates a root where all react dom manipulations take place
-        root.render(heading); //converts the object created by React.createElement into html tag that the browser can understand
---
+++ Readme.md
@@ -0,0 +1,9 @@
+Namaste React
+
+<!-- ~ knows as tilde basically tells that the version will only have major updates of parcel
+^ knows as caret basically tells us that version will have minor updates of parcel -->
+<!-- //    const heading = document.createElement("h1");
+        //    heading.innerText = "SAHIL GOAT CODER";
+
+        //    const root = document.getElementById("root");
+        //    root.append(heading); -->
