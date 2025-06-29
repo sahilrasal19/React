f5534bfaabf16f2e82d498828f498cd3538822fa
SAHIL <sahilrasal742@gmail.com>
2025-06-29 18:21:30 +0530

    3rd episode updates

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+const head = <p>rohit sharma</p>
+const Title = () => (
+        <p>Virat kohli Goat</p>
+)
+const number = 1000;
 const Heading2 = () => (
         <div>
                 <h2>Sahil loves React</h2>
@@ -11,6 +16,10 @@ const Heading1 = () => (
         <div>
                 <h1>Sahil loves JS</h1>
                 <Heading2/>
+                {Title()} 
+                {head}
+                {number}
+                { 100+200}  
         </div>
 )
 
