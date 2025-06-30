b0db0b589932e1770166770f888419805c9a0487
SAHIL <sahilrasal742@gmail.com>
2025-06-30 14:05:10 +0530

    4th epsiode half

--- App.js
+++ App.js
@@ -1,27 +1,74 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const head = <p>rohit sharma</p>
-const Title = () => (
-        <p>Virat kohli Goat</p>
+/*
+Header
+*/
+
+const Header = () => {
+return (
+        <div className="header">
+                <div className="header_logo">
+                        <img className = "logo"  
+                        src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCTqVNowhzVuJsQ-D6VGFEQ_g5-uIDYQyU6Q&s"/>
+                </div>
+                <div className="Navbar">
+                        <ul>
+                                <li>Home</li>
+                                <li>About Us</li>
+                                <li>Cart</li>
+                                <li>Contact Us</li>
+                        </ul>
+                </div>
+        </div>
 )
-const number = 1000;
-const Heading2 = () => (
-        <div>
-                <h2>Sahil loves React</h2>
-                <h3>Sahil loves jsx</h3>
-        </div> 
+}
+
+const RestaurantCards = () => {
+return (
+        <div className="resto_cards">
+                <div className="food_image">
+                        <img className="food_logo"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpdXryjL4CrgeSA3lgHfai01rbj1gVHY0zIg&s">
+                        </img>
+                </div>
+                        <div className="food_details">
+                                <h3>Spring Fields</h3>
+                                <h4>Pav Bhaji Special</h4>
+                                <h4>4.4 Stars</h4>
+                                <h4>38 Minutes</h4>
+                        </div>
+                                
+                     
+                
+        </div>
 )
-const Heading1 = () => (
+}
+
+const Body = () => {
+return (
+        <div className="body">
+                <div className="search">
+                        Search
+                </div>
+                <div className="resto-container">
+                        <div>
+                                <RestaurantCards/>
+                        </div>
+                </div>
+        </div>
+)
+}
+
+const AppLayout = () => {
+return (
         <div>
-                <h1>Sahil loves JS</h1>
-                <Heading2/>
-                {Title()} 
-                {head}
-                {number}
-                { 100+200}  
+                <Header/>
+                <Body/>
         </div>
 )
 
+}
+
 const root = createRoot(document.getElementById("root"));
-root.render(<Heading1/>);
+root.render(<AppLayout/>);
